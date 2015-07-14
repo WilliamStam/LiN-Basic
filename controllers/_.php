0ae9809f65b2bbd990e1789ace76d6eb37b620b6
William Stam <william@impreshin.com>
2015-07-14 12:41:24 +0200

    Latest

---
+++ controllers/_.php
@@ -0,0 +1,16 @@
+<?php
+namespace controllers;
+use \timer as timer;
+class _ {
+
+	function __construct() {
+		$this->f3 = \base::instance();
+		$this->user = $this->f3->get("user");
+
+		
+		
+	}
+
+
+	
+}
---
+++ controllers/home.php
@@ -0,0 +1,31 @@
+<?php
+namespace controllers;
+use \timer as timer;
+class home extends _ {
+	function __construct(){
+		parent::__construct();
+		
+	}
+	function page(){
+		$user = $this->f3->get("user");
+		
+		
+		echo "test";
+		
+		/*
+		
+		$tmpl = new \template("template.twig");
+		$tmpl->page = array(
+			"section"    => "home",
+			"sub_section"=> "home",
+			"template"   => "home",
+			"meta"       => array(
+				"title"=> "MeetPad",
+			),
+			"css"=>"",
+			"js"=>"",
+		);
+		$tmpl->output();
+		*/
+	}
+}
