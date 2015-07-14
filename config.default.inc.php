4cee3ce28df8c0569e1af28d998ca25f611934b7
William Stam <william@impreshin.com>
2015-07-14 12:49:31 +0200

    Latest

--- config.default.inc.php
+++ config.default.inc.php
@@ -4,6 +4,6 @@
 $cfg['git'] = array(
 	'username'=>"",
 	"password"=>"",
-	"path"=>"github.com/WilliamStam/MeetPad-v2.git",
+	"path"=>"github.com/WilliamStam/LiN-Basic.git",
 	"branch"=>"master"
 );
